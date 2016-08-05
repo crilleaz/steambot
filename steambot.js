5782e1e748aa3ef1e2e341c53edaad456aa38ac0
jmlattanzi <jonathanlattanzi@gmail.com>
2016-08-04 23:30:37 -0700

    temporary markov chain generator added

--- steambot.js
+++ steambot.js
@@ -1,18 +1,18 @@
-/*
-    use fs to log received messages in array format in a file for the bot to choose at random
-    eventually work on defining  sentence structure/grammer for more realistic replies
-*/
-
 // bot authored by Jonathan Lattanzi
 // thanks to DoctorMcKay and seishun for creating the node-steam libraries
 
 var steam_user = require('steam-user')
 var client = new steam_user()
-var fs = require('fs')
+
+var util = require('util')
+var file = require('fs')
+var mark = require('markovchain-generate')
+
+var chain = new mark()
 
 // log in creds
 client.logOn({
-  'accountName': 'jmlattanzi',
+  'accountName': 'salad_bot',
   'password': ''
 })
 
@@ -43,18 +43,15 @@ client.on('friendMessage', function(steamID, message){
   reply(steamID, message)
 })
 
-var replies = [
-  'kek', 'topkek', 'nice meme', 'yee', 'im not sure how i feel about that', 'no',
-  'i really cant believe how tall giraffes are', 'i want you to hit me as hard as you can',
-  'im not sure how i feel about car jackers',
-  'lol ok', 'ikr', 'fugg', 'damn son, whered ya find this'
-]
+// temp work around until i make my own markov chain generator
+function reply(steamID, message) {
+  var file = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-// main reply function
-function reply(steamID,  message){
-  var reply_msg = replies[Math.floor(Math.random() * replies.length)]
+  chain.generateChain(file)
+  var reply_msg = chain.generateString()
+
   client.chatMessage(steamID, reply_msg)
-  console.log('[sent:] ' + reply_msg)
+  console.log('[sent:]' + reply_msg)
 }
 
 // echo message once for every character in the received message
